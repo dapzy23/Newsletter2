206e465cefaf80fa963c4023e94f8c53588b6c11
dapzy23 <akundapzzy@gmail.com>
2026-09-20 12:13:34 +0800

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,9 @@
+{
+  "newsletterIds": [
+    "120363410914213919@newsletter",
+    "120363409704581339@newsletter",
+    "120363430605484588@newsletter",    
+    "120363418538598013@newsletter"
+    
+  ]
+    }
